3d68458e326441d71776d62c20ed237ceb1e018a
Tabrez Khan <146086546+tabrezkhan005@users.noreply.github.com>
2025-02-12 12:34:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { Component, useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// Component import
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import FinancialCalculator from "./components/FinancialCalculator/FinancialCalculator";
+import CarList from "./components/CarList/CarList";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  // dark mode start
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+  // dark mode end
+
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Hero theme={theme} />
+      <About />
+      <Services />
+      <FinancialCalculator/>
+      <CarList />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
